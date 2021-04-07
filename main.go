2c4dad5bd90563256b2877fe792250dbdddded49
Mitchell de Bruyn <mitch@physicscat.org>
2021-04-07 09:20:52 +0200

    add example to use second config file

--- main.go
+++ main.go
@@ -24,19 +24,24 @@ func mainRun(c *cli.Context) error {
 func main() {
 	flags := []cli.Flag{
 		altsrc.NewIntFlag(&cli.IntFlag{Name: "loglevel", Aliases: []string{"l"}, Value: 3}),
-		&cli.StringFlag{
-			Name:       "config",
-			Value:      "eg_config.yaml",
-			HasBeenSet: true,
-		},
+		&cli.StringFlag{Name: "config", Value: "eg_config.yaml", HasBeenSet: true},
+		&cli.StringFlag{Name: "global-config", Value: "eg_global-config.yaml", HasBeenSet: true},
 	}
 
 	before := func(c *cli.Context) error {
+		// Command line flags always overwrite configuration files
 		first := altsrc.InitInputSourceWithContext(flags, altsrc.NewYamlSourceFromFlagFunc("config"))
 		err := first(c)
 		if err != nil {
 			klog.Error(err)
 		}
+		// The second config map will not overwrite the first
+		second := altsrc.InitInputSourceWithContext(flags, altsrc.NewYamlSourceFromFlagFunc("global-config"))
+		err = second(c)
+		if err != nil {
+			klog.Error(err)
+		}
+
 		fs := flag.NewFlagSet("", flag.PanicOnError)
 		klog.InitFlags(fs)
 		return fs.Set("v", strconv.Itoa(c.Int("loglevel")))
